dbab504e287ef5e1f2801f142a4b7ac20ea3c97e
shunthirimay89 <shunmay8999@gmail.com>
2024-04-28 07:28:01 +0630

    Rest Api

---
+++ STRMDotNetCoreRestApi/Controllers/BlogController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace STRMDotNetCoreRestApi.Controllers
+{
+    //endPoint
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        [HttpGet]
+        public  IActionResult Read() 
+        {
+            return Ok("Read");
+        }
+
+        [HttpPost]
+        public IActionResult Create()
+        {
+            return Ok("Read");
+        }
+
+
+        [HttpPut]
+        public IActionResult Update()
+        {
+            return Ok("Update");
+        }
+
+
+        [HttpPatch]
+        public IActionResult Patch()
+        {
+            return Ok("Patch");
+        }
+
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok("Delete");
+        }
+    }
+}
